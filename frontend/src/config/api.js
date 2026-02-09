ae9264e05ea7c7d05e9b9ab467fc3b22eb79b8f5
Fantasar <lapique.philippe@gmail.com>
2026-02-09 12:04:55 +0100

    feat: Ajout du fichier de configuration pour l'url du front-end de Vite

---
+++ frontend/src/config/api.js
@@ -0,0 +1,9 @@
+// frontend/src/config/api.js
+
+/**
+ * Configuration centralisée de l'API
+ */
+
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5500';
+
+export default API_URL;
